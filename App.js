a5505d04741e2c6fddb4c11e051343211bba79c4
Lucas Lemos <124939307+LukassLemos@users.noreply.github.com>
2023-07-30 00:11:51 -0300

    Initial commit

---
+++ App.js
@@ -0,0 +1,117 @@
+import React, { Component } from 'react';
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  Image,
+  ImageBackground
+  } from 'react-native';
+
+class App extends Component{
+
+  constructor(props){
+    super(props);
+    this.state = {
+      textoFrase: '',
+      img: require('./src/biscoito.png'),
+    };
+
+    this.quebraBiscoito = this.quebraBiscoito.bind(this);
+
+    this.frases = [
+      'Siga os bons e aprenda com eles.', 
+      'O bom-senso vale mais do que muito conhecimento.', 
+      'O riso é a menor distância entre duas pessoas.', 
+      'Deixe de lado as preocupações e seja feliz.',
+      'Realize o óbvio, pense no improvável e conquiste o impossível.',
+      'Acredite em milagres, mas não dependa deles.',
+      'A maior barreira para o sucesso é o medo do fracasso.'
+    ];
+
+  }
+
+  quebraBiscoito(){
+    let numeroAleatorio = Math.floor(Math.random() * this.frases.length );
+
+    this.setState({
+      textoFrase: ' "' + this.frases[numeroAleatorio] + '" ',
+      img: require('./src/biscoitoAberto.png')
+    });
+  
+  }
+
+
+  render(){
+    return(
+      <View style={styles.container} > 
+
+      <Text style={styles.textotopo}>
+        Biscoito da Sorte
+      </Text>
+      
+      <Image
+        source={this.state.img}
+        style={styles.img}
+      />
+
+      <Text style={styles.textoFrase}>{this.state.textoFrase}</Text>
+
+      <TouchableOpacity style={styles.botao} onPress={this.quebraBiscoito}>
+        <View style={styles.btnArea}>
+          <Text style={styles.btnTexto}>Quebrar Biscoito</Text>
+        </View>
+      </TouchableOpacity>  
+
+      </View>    
+    );
+  }
+
+}
+
+const styles = StyleSheet.create({
+  container:{
+    flex:1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#C0C0C0'
+  },
+  img:{
+    width: 250,
+    height: 250,
+  },
+  textotopo:{
+    fontSize: 30,
+    fontStyle: "italic",
+    margin: 50
+  },
+  textoFrase:{
+    fontSize: 20,
+    color: 'black',
+    margin: 20,
+    fontStyle: 'italic',
+    textAlign: 'center'
+  },
+  botao:{
+    width: 230,
+    height: 50,
+    borderWidth: 2,
+    backgroundColor: 'black',
+    borderColor: '#dd7b22',
+    borderRadius: 25 
+  },
+  btnArea:{
+    flex:1,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center'
+
+  },
+  btnTexto:{
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: '#dd7b22'
+  }
+});
+
+export default App;
